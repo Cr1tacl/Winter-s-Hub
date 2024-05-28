07807bde44bfe2914530eaa45924cba662374de3
Cr1tacl <152733429+Cr1tacl@users.noreply.github.com>
2024-05-27 18:02:42 -0700

    Update Main Script.lua
    fix

--- Main Script.lua
+++ Main Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Cr1tacl/Winter-s-Hub/main/grabmainscript"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Cr1tacl/Winter-s-Hub/main/grabmainscript.lua"))()
