bea30420b6d0f1ba048aef624d437b46a25170c8
Cr1tacl <152733429+Cr1tacl@users.noreply.github.com>
2024-05-28 19:27:29 -0700

    Update reliefui.lua
    added bunch

--- reliefui.lua
+++ reliefui.lua
@@ -13,4 +13,39 @@ relief.addModule("Movement", "speedhax", function(Toggled)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Toggled and 50 or 16
 end)
 
---credit to toungegotyourcat on rblx
+relief.addModule("Exploit", "infinite yield", function(Toggled)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end)
+
+relief.addModule("Exploit", "Internal UI (for solara)", function()
+	loadstring(game:HttpGet('loadstring(game:HttpGet("https://pastebin.com/raw/xsPhFys2"))()'))()
+end)
+
+relief.addModule("Player", "Spider Man (R15)" , function()
+	loadstring(game:HttpGet('https://pastebin.com/raw/zXk4Rq2r'))()
+end)
+
+relief.addModule("Player", "Spider Man (R6)" , function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/2X0hKUgq'),true))()
+end)
+
+relief.addModule("Player", "R15 To R6 (FE)" , function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Imagnir/r6_anims_for_r15/main/r6_anims.lua'),true))()
+end)
+
+relief.addModule("FE Emote", "FE Emote (use ,)" , function()
+	--keybind to open is comma
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Gi7331/scripts/main/Emote.lua"))()
+end)
+
+relief.addModule("Render", "FpsUnlocker Max" , function()
+	setfpscap(999)
+end)
+
+relief.addModule("World", "SimpleSpy" , function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/hUSH0zgt"))()
+end)
+
+relief.addModule("World", "Hydroxide" , function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/SqfkEjyj"))()
+end)
